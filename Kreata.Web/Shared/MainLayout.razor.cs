b0b4c2801a5749f63d696e8d5fbb67ddeb4c9647
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-02-01 16:54:07 +0100

    Sötét, világos és aktuális témák

--- Kreata.Web/Shared/MainLayout.razor.cs
+++ Kreata.Web/Shared/MainLayout.razor.cs
@@ -1,9 +1,14 @@
+using MudBlazor;
+
 namespace Kreata.Web.Shared
 {
     public partial class MainLayout
     {
         private bool _drawerOpen = true;
         private bool _isCurrentLightTheme = true;
+        private MudTheme _darkMudTheme = new MudTheme();
+        private MudTheme _lightMudTheme = new MudTheme();
+        private MudTheme _currentTheme = new MudTheme();
         private void DrawerToggle()
         {
             _drawerOpen = !_drawerOpen;
