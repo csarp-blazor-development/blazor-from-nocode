27edf2f919dbb580c83e2856dd2c9ccbe9dd00c5
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-02-01 16:55:57 +0100

    Legyenek metódusaink, amelyek beállítják a világos és sötét palettát

--- Kreata.Web/Shared/MainLayout.razor.cs
+++ Kreata.Web/Shared/MainLayout.razor.cs
@@ -13,5 +13,64 @@ namespace Kreata.Web.Shared
         {
             _drawerOpen = !_drawerOpen;
         }
+
+        private void InitializeDarkTheme()
+        {
+            _darkMudTheme = new MudTheme()
+            {
+                Palette = new PaletteDark
+                {
+                    AppbarBackground = "#0097FF",
+                    AppbarText = "#FFFFFF",
+
+                    Background = "#001524",
+
+                    Primary = "#007CD1",
+                    Secondary = "#117CE1",
+                    Tertiary = "#107C51",
+
+
+                    TextPrimary = "#FFFFFF",
+                    TextSecondary = "#FEFEFE",
+
+                    DrawerBackground = "#093958",
+                    DrawerText = "#FFFFFF",
+                    Surface = "#093958",
+
+                    ActionDefault = "#0097FF",
+                    ActionDisabled = "#2F678C",
+                    TextDisabled = "#B0B0B0"
+                }
+            };
+        }
+
+        private void InitializeLightTheme()
+        {
+            _lightMudTheme = new MudTheme()
+            {
+                Palette = new PaletteLight
+                {
+                    AppbarBackground = "#0097FF",
+                    AppbarText = "#FFFFFF",
+
+                    Background = "#F4FDFF",
+
+                    Primary = "#007CD1",
+                    Secondary = "#117CE1",
+                    Tertiary = "#107C51",
+
+                    TextPrimary = "#0C1217",
+                    TextSecondary = "#0C1217",
+
+                    DrawerBackground = "#C5E5FF",
+                    DrawerText = "#0C1217",
+                    Surface = "#E4FAFF",
+
+                    ActionDefault = "#0C1217",
+                    ActionDisabled = "#2F678C",
+                    TextDisabled = "#676767"
+                }
+            };
+        }
     }
 }
